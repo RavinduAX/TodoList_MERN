2a18efee972d2e0fbb71b019cfa10cff4fd86a73
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-09-07 14:43:15 +0530

    get data to frontend done

--- frontend/src/components/Todobody.jsx
+++ frontend/src/components/Todobody.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Todobox from './Todobox'
+import axios from 'axios'
 
 const Todobody = () => {
+   const [todos, setTodos] = useState([])
+
+   useEffect(() => {
+      getAllTodos();
+   }, [])
+
+   const getAllTodos = () => {
+      axios.get('http://localhost:5000/api/v1/todo/')
+         .then((res) => {
+            if (res.data.success) {
+               setTodos(res.data.payload)
+            }
+         })
+         .catch((err) => {
+            console.log(err)
+         })
+   }
+
    return (
       <div>
          <div className='border-2 border-gray-600 flex justify-between mb-12'>
@@ -9,7 +28,9 @@ const Todobody = () => {
             <button className='py-1 px-6 bg-gray-600 text-white'>Add</button>
          </div>
          <div className='overflow-y-scroll max-h-[20rem]'>
-            <Todobox />
+            {todos.map((todo) => (
+               <Todobox todo={todo} />
+            ))}
          </div>
 
       </div>
