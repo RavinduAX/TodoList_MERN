f5e6094a8f3872be160097032f958c7309b61a61
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-09-07 14:56:44 +0530

    save todo created

--- frontend/src/components/Todobody.jsx
+++ frontend/src/components/Todobody.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const Todobody = () => {
    const [todos, setTodos] = useState([])
+   const [text, setText] = useState('')
 
    useEffect(() => {
       getAllTodos();
@@ -21,15 +22,32 @@ const Todobody = () => {
          })
    }
 
+   //add todos
+   const handleAddClick = (e) => {
+      e.preventDefault();
+      const newTodo = {'todo':text}
+      axios.post('http://localhost:5000/api/v1/todo/', newTodo)
+         .then((res) => {
+            if (res.data.success) {
+               alert(res.data.msg)
+               getAllTodos();
+               setText('')
+            }
+         })
+         .catch((err) => {
+            console.log(err)
+         })
+   }
+
    return (
       <div>
          <div className='border-2 border-gray-600 flex justify-between mb-12'>
-            <input type="text" placeholder='Add your task' className='w-full focus:outline-none p-2' />
-            <button className='py-1 px-6 bg-gray-600 text-white'>Add</button>
+            <input onChange={(e) => { setText(e.target.value) }} type="text" placeholder='Add your task' className='w-full focus:outline-none p-2' />
+            <button onClick={handleAddClick} className='py-1 px-6 bg-gray-600 text-white'>Add</button>
          </div>
          <div className='overflow-y-scroll max-h-[20rem]'>
             {todos.map((todo) => (
-               <Todobox todo={todo} />
+               <Todobox key={todo._id} todo={todo} />
             ))}
          </div>
 
